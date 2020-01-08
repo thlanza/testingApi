66e2d180610b7867d02a30f2f4d58413c83c6ec5
thlanza <thlanza@hotmail.com>
2020-01-08 19:45:28 -0200

    primeiro commit

---
+++ src/config/routes.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+  app
+    .route("/users")
+    .get(app.routes.users.findAll)
+    .post(app.routes.users.create);
+
+  app
+    .route("/accounts")
+    .get(app.routes.accounts.findAll)
+    .post(app.routes.accounts.create);
+
+  app.route("/accounts/:id").get(app.routes.accounts.get);
+};
---
+++ src/services/user.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+  const findAll = (filter = {}) => {
+    return app
+      .db("users")
+      .where(filter)
+      .select();
+  };
+
+  const save = async user => {
+    if (!user.name) return { error: "Nome é um atributo obrigatório" };
+    if (!user.mail) return { error: "Email é um atributo obrigatório" };
+    if (!user.passwd) return { error: "Senha é um atributo obrigatório" };
+
+    const userDb = await findAll({ mail: user.mail });
+    if (userDb && userDb.length > 0)
+      return { error: "Já existe um usuário com esse email" };
+    return app.db("users").insert(user, "*");
+  };
+
+  return { findAll, save };
+};
